ef71881fa739120e8cbbcada70934943682c6d49
Mateusz-Sadowski <126981797+Mateusz-Sadowski@users.noreply.github.com>
2023-11-05 00:41:34 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,14 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource *res;//W ten sposób przechowujemy wskaźnik do miejsca pamięci, w którym faktycznie znajduje się Resource.
+    //Oznacza to, że nie musimy go przechowywać w przestrzeni pamięci wewnątrz tej klasy, tylko możemy posiadać niewielkie odniesienie. 
+public:
+    ResourceManager() : res(new Resource()) {}//Wewnątrz konstruktora domyślnego ResourceManager jest wywoływany konstruktor domyślny klasy Resource.
+    //Przy samej inicjalizacji tworzymy "zmienną" typu Resource (konstruktor domyślny).
+    double get() { return res->get(); }//Pierwsza metoda get należy do klasy ResourceManager a drugie get należy do struktury Resource res.
+    //Ponieważ jest to struktura a nie klasa, więc zamiast kropki jest strzałka.
+
+    ~ResourceManager() { delete res; }
 };
