1260eca15d54454099f3c698c74ad754b52e7ad1
Mateusz-Sadowski <126981797+Mateusz-Sadowski@users.noreply.github.com>
2023-11-05 01:11:22 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -11,4 +11,5 @@ public:
     double get() { return res->get(); }
     ResourceManager(const ResourceManager& rm):res(rm.res) {}
     ~ResourceManager() { delete res; }
+    ResourceManager& operator=(const ResourceManager& rm) { res = rm.res; return *this; }
 };
